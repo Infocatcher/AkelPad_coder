8bb27254e004cba51fc76437a2e1409a16499d64
Infocatcher <infocatcher.dev.null@gmail.com>
2015-10-18 20:43:42 +0300

    Testcase for regular expressions in JavaScript Note: currently not all tests are passed

---
+++ _js.coder.test.js
@@ -0,0 +1,32 @@
+// Regular expressions
+var r = /\//i;
+var r = /[\/*]/img;
+var r = /[a-z]{5}/;
+var r = /[a-z]{1,5}/;
+var r = /([a-z])+/;
+var r = /([a-z]{1,5})+/;
+var r = /f([a-z])/;
+var r = /a(b)c(d)/;
+if(/\*/.test(s));
+/./.test(s) && f();
+/[a-z]/.test(s) && f();
+var a = [/a/, /b/];
+var o = { r: /./, x: 2 };
+var x = a && /b/.test(c);
+var x = a ? /b/ : /c/;
+var x = !/a/.test(b);
+var x = f(/./i);
+
+// Not a regular expressions
+var n = 1/2;
+var n = 1/2/3;
+var n = 1/2/i;
+var n = 1/(2/i);
+var n = 1/this.n/3;
+var n = 1/this.n()/3;
+var n = 1/this.n(2/3);
+var n = 1/2 + (function() { return 3/4; })();
+var n = 1/2*(this.x/3 + 2);
+var n = 1/2*(this.x + 2/3);
+var n = f(1/2) + 3/2;
+var n = f(1/2) + g(3/2);
